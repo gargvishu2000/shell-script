75dbeb16c3a0a86eb054e7a12cf6dba524353135
Subham Garg <Subham.Garg@mobileum.com>
2022-12-15 21:15:43 +0530

    first commit

--- script/shell-script.sh
+++ script/shell-script.sh
@@ -8,12 +8,11 @@ aws s3 cp s3://antora/docs s3://softwaredocs/Portal/modules/ROOT/pages/  --recur
 echo "2"
 fi
 
-# keycloak-path='aws s3 ls s3://softwaredocs/Keycloak/modules/ROOT/pages'
 if [[ $(aws s3 ls s3://softwaredocs/Keycloak/modeules/ROOT/pages | head) ]]; then
-aws s3 cp s3://antora/docs/index.adoc s3://softwaredocs/Keycloak/modules/ROOT/pages/ --recursive
+aws s3 cp s3://antora/docs/index.adoc s3://softwaredocs/Keycloak/modules/ROOT/pages/ 
 echo "3"
 else
-aws s3 cp s3://antora/docs/index.adoc s3://softwaredocs/Keycloak/modules/ROOT/pages/ --recursive
+aws s3 cp s3://antora/docs/index.adoc s3://softwaredocs/Keycloak/modules/ROOT/pages/ 
 echo "4"
 fi
 # npx antora antora-playbook.yml
