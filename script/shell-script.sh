4c8f1b86995d7d65bde4e7677471f2383b3b5ab7
Subham Garg <Subham.Garg@mobileum.com>
2022-12-15 19:36:40 +0530

    first commit

--- script/shell-script.sh
+++ script/shell-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-path=' aws s3 ls s3:softwaredocs/Portal'
+path=' aws s3 ls s3:softwaredocs/Portal/modeules/ROOT/pages'
 if [[ -z path ]]; then
 aws s3 cp s3://antora/docs s3://softwaredocs/  --recursive
 else
-aws s3 cp s3://antora/docs s3://softwaredocs/Portal/  --recursive
+aws s3 cp s3://antora/docs s3://softwaredocs/Portal/modules/ROOT/pages  --recursive
 fi
