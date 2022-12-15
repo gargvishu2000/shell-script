8a392c614d6ca349b972d5508f7bbb343ed6e35b
Subham Garg <Subham.Garg@mobileum.com>
2022-12-15 17:04:01 +0530

    first commit

--- script/shell-script.sh
+++ script/shell-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 path=' aws s3 ls s3:softwaredocs/Portal'
 if [[ -z path ]]; then
-aws s3 cp s3://antora/docs/arguments.adoc s3://softwaredocs/
+aws s3 cp s3://antora/docs/ s3://softwaredocs/
 else
-aws s3 cp s3://antora/docs/arguments.adoc s3://softwaredocs/Portal/
+aws s3 cp s3://antora/docs/ s3://softwaredocs/Portal/
 fi
