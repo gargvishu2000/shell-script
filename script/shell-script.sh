4a9377a9ed1df321992cddc5870fccdee4110e09
Subham Garg <Subham.Garg@mobileum.com>
2022-12-15 20:58:36 +0530

    first commit

--- script/shell-script.sh
+++ script/shell-script.sh
@@ -3,13 +3,13 @@ portal-path=' aws s3 ls s3:softwaredocs/Portal/modeules/ROOT/pages'
 if [[ -z portal-path ]]; then
 aws s3 cp s3://antora/docs s3://softwaredocs/  --recursive
 else
-aws s3 cp s3://antora/docs s3://softwaredocs/Portal/modules/ROOT/pages  --recursive
+aws s3 cp s3://antora/docs s3://softwaredocs/Portal/modules/ROOT/pages/  --recursive
 fi
 
 keycloak-path='aws s3 ls s3://softwaredocs/Keycloak/modules/ROOT/pages'
 if [[ -z keycloak-path ]]; then
-aws s3 cp s3://antora/docs/index.adoc s3://softwaredocs/Keycloak/modules/ROOT/pages --recursive
+aws s3 cp s3://antora/docs/index.adoc s3://softwaredocs/Keycloak/modules/ROOT/pages/ --recursive
 else
-aws s3 cp s3://antora/docs/index.adoc s3://softwaredocs/Keycloak/modules/ROOT/pages --recursive
+aws s3 cp s3://antora/docs/index.adoc s3://softwaredocs/Keycloak/modules/ROOT/pages/ --recursive
 fi
 # npx antora antora-playbook.yml
