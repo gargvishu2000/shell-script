7714449393367ed8b440d20a46e7216cacdbc18d
Subham Garg <Subham.Garg@mobileum.com>
2022-12-15 23:40:19 +0530

    first commit

--- script/shell-script.sh
+++ script/shell-script.sh
@@ -15,15 +15,10 @@ else
 aws s3 cp s3://antora/docs/index.adoc s3://softwaredocs/Keycloak/modules/ROOT/pages/
 echo "4"
 fi
-apt install curl
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
-nvm install v16
-nvm use 16.14.2
-sudo apt -y install gcc g++ make
-sudo apt install gnupg2
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-apt update && sudo apt install yarn
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+source ~/.bashrc
+nvm install v16.2.0
 node -e "fs.writeFileSync('package.json', '{}')"
 npm i -D -E @antora/cli@3.1 @antora/site-generator@3.1
 npx antora --fetch antora-playbook.yml
