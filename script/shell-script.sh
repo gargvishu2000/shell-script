0ae6e8c02b3260a9214226464d979b4742a120aa
Subham Garg <Subham.Garg@mobileum.com>
2022-12-15 21:04:43 +0530

    first commit

--- script/shell-script.sh
+++ script/shell-script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 portal-path=' aws s3 ls s3:softwaredocs/Portal/modeules/ROOT/pages'
-if [[ -z portal-path ]]; then
+if [[ $(aws s3 ls s3:softwaredocs/Portal/modeules/ROOT/pages | head ) ]]; then
 aws s3 cp s3://antora/docs s3://softwaredocs/  --recursive
 echo "1"
 else
@@ -8,12 +8,12 @@ aws s3 cp s3://antora/docs s3://softwaredocs/Portal/modules/ROOT/pages/  --recur
 echo "2"
 fi
 
-keycloak-path='aws s3 ls s3://softwaredocs/Keycloak/modules/ROOT/pages'
-if [[ -z keycloak-path ]]; then
-aws s3 cp s3://antora/docs/index.adoc s3://softwaredocs/Keycloak/modules/ROOT/pages/ --recursive
-echo "3"
-else
-aws s3 cp s3://antora/docs/index.adoc s3://softwaredocs/Keycloak/modules/ROOT/pages/ --recursive
-echo "4"
-fi
+# keycloak-path='aws s3 ls s3://softwaredocs/Keycloak/modules/ROOT/pages'
+# if [[ -z keycloak-path ]]; then
+# aws s3 cp s3://antora/docs/index.adoc s3://softwaredocs/Keycloak/modules/ROOT/pages/ --recursive
+# echo "3"
+# else
+# aws s3 cp s3://antora/docs/index.adoc s3://softwaredocs/Keycloak/modules/ROOT/pages/ --recursive
+# echo "4"
+# fi
 # npx antora antora-playbook.yml
